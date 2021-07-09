02e4ff710d267903d7481871c0d3924bd2d2d2fc
Nikolai Sokol <58706116+webunreal@users.noreply.github.com>
2021-07-09 14:33:14 +0300

    Delete SwiftObjC directory

--- SwiftObjC/AnotherObjCLib/AnotherObjCLibUmbrellaHeader.h
+++
@@ -1,8 +0,0 @@
-//
-//  AnotherObjCLibUmbrellaHeader.h
-//  SwiftObjC
-//
-//  Created by Nikolai Sokol on 08.07.2021.
-//
-
-#import "AnotherObjCLib.h"
--- SwiftObjC/ObjCLib/ObjCLibUmbrellaHeader.h
+++
@@ -1,8 +0,0 @@
-//
-//  ObjCLibUmbrellaHeader.h
-//  SwiftObjC
-//
-//  Created by Nikolai Sokol on 08.07.2021.
-//
-
-#import "ObjCLib.h"
